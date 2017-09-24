0c9be2e819c8b05b2056f5a81f13c35f062b1fb0
David Basil <info@basildavid.com>
2017-09-24 15:12:46 +0400

    add new user functionality

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
 var express = require('express')
 var router = express.Router()
+var user = require('../models/user')
 
-// register
+// register get
 router.get('/register', function(req, res){
 	res.render('register')
+})
+
+// register post
+router.post('/register', function(req, res){
+	var name = req.body.name
+	var email = req.body.email
+	var username = req.body.username
+	var password = req.body.password
+	var password2 = req.body.password2
+	// validation
+	req.checkBody('name', 'Name is required').notEmpty()
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+	var errors = req.validationErrors()
+	if (errors) {
+		res.render('register', {
+			errors: errors
+		})
+	} else {
+		var newUser = new User({
+			name: name,
+			email: email,
+			username: username,
+			password: password
+		})
+		User.createUser(newUser, function(err, user){
+			if (err) throw err
+			console.log(user)
+		})
+		req.flash('success_msg', 'You are registered and can now log in')
+		res.redirect('/users/login')
+	}
 })
 
 // login
